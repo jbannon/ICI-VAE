2d32fe8e4865aac7ccb83c332688abdb4386b03a
James Bannon <j.bannon90@gmail.com>
2023-04-01 21:51:51 -0400

    skeleton VAE class

---
+++ src/EncoderVAEs.py
@@ -0,0 +1,33 @@
+import torch as nn 
+from torch.nn import Linear, ReLU
+from torch_geometric.nn import Sequential, GCNConv
+
+
+
+
+class BaseVAE:
+	def __init__(self):
+		pass
+
+	def forward(self):
+		pass 
+
+	def encode(self):
+
+class VanillaVAE(nn.Module):
+	def __init__(self):
+		pass
+
+
+
+
+class ConditionalVAE(nn.module):
+	def __init__(self):
+		pass
+
+
+
+
+class GraphConditionalVAE(nn.module):
+	def __init__(self):
+		pass
